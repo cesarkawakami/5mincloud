3bc4891360ec7b0a1517e58ea5eeecee8a61dcc6
Cesar Kawakami <cesar@geekie.com.br>
2013-10-02 14:40:27 -0700

    wow db

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 import os, flask
 app = flask.Flask(__name__)
 
-@app.route("/")
+import pymongo
+db = pymongo.MongoClient(
+    os.environ.get("MONGOLAB_URI", "mongodb://localhost/5mincloud")
+).get_default_database()
+
+@app.route("/", methods=["GET"])
 def hello():
-    return "Hello, world!"
+    messages = db.messages.find()
+    return flask.render_template("hello.html", messages=messages)
+
+@app.route("/", methods=["POST"])
+def post():
+    db.messages.insert({
+        "message": flask.request.form["message"]
+    })
+    return flask.redirect("/")
 
 if __name__ == "__main__":
     app.run(
